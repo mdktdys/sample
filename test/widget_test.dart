26e4bc58077c4fcda4ec4339731c34f2b764613c
mdktdys <mdktdys@MacBook-Pro-de-Danil.local>
2025-02-04 21:40:54 +0500

    green

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 
 import 'package:flutter_test/flutter_test.dart';
@@ -19,6 +12,7 @@ void main() {
     // setup
     await tester.pumpWidget(const MyApp());
     final button = find.byType(FloatingActionButton).first;
+
 
     // action
     await tester.tap(button);
